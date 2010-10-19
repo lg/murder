06192a216294eef9662fe29ab4308ee999581e28
Larry <trivex@trivex.net>
2010-10-19 14:44:11 -0700

    fix scenario where the machine sending the dist package is one of the recipients. thanks to robinbowes for reporting this bug.

--- lib/murder/admin.rb
+++ lib/murder/admin.rb
@@ -26,11 +26,11 @@ namespace :murder do
 
     # TODO: Skip hidden (.*) files
     # TODO: Specifyable tmp file
-    system "tar -c -z -C #{dist_path} -f /tmp/murder_dist.tgz ."
-    upload("/tmp/murder_dist.tgz", "/tmp/murder_dist.tgz", :via => :sftp)
+    system "tar -c -z -C #{dist_path} -f /tmp/murder_dist_to_upload.tgz ."
+    upload("/tmp/murder_dist_to_upload.tgz", "/tmp/murder_dist.tgz", :via => :sftp)
     run "tar xf /tmp/murder_dist.tgz -C #{remote_murder_path}"
     run "rm /tmp/murder_dist.tgz"
-    system "rm /tmp/murder_dist.tgz"
+    system "rm /tmp/murder_dist_to_upload.tgz"
   end
 
   desc "Starts the Bittorrent tracker (essentially a mini-web-server) listening on port 8998."
